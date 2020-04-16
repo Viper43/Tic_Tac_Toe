ebe35265502f6395e9903aa3121f66e88b93b132
Viper43 <59294267+Viper43@users.noreply.github.com>
2020-04-16 20:04:17 +0530

    Update Tic_Tac_Toe_main.java

--- Tic_Tac_Toe_main.java
+++ Tic_Tac_Toe_main.java
@@ -1,6 +1,8 @@
 import java.util.*;
 public class Tic_Tac_Toe_main {
+	
 	static char matrix[][] = new char[3][3];
+	static boolean winner = false;
 	
 	public static void display() {
 		for(int i = 0; i < 3; i++ ) {
@@ -14,7 +16,6 @@ public class Tic_Tac_Toe_main {
 			if( i < 2 )
 				System.out.println("--------------------------------");
 		}
-		check_Move();
 	}
 	
 	public static void move(char c) {
@@ -24,30 +25,61 @@ public class Tic_Tac_Toe_main {
 		int row = sc.nextInt();
 		int column = sc.nextInt();
 		
+		row--;
+		column--;
+		
 		if( matrix[row][column] == 'X' || matrix[row][column] == 'O' ) {
 			System.out.println("Are you illiterate?????? Giving you another try");
 			move(c);
 		}
-		else
+		else {
 			matrix[row][column] = c;
-		
-		display();
-	}
-	
-	public static void check_Move() {
-		for(int i = 0; i < 3; i++ ) {
-			for(int j = 0; j < 3; j++ ) {
-				
-			}
+			display();
 		}
 	}
 	
-	public static void main(String args[]) {
-		for(int i = 1; i <= 9; i++ ) {
-			if( i % 2 == 0 )
-				move('O');
+	public static void check_Move(char c) {
+		int rowCount = 0, columnCount = 0, leftDiagonalCount = 0, rightDiagonalCount = 0;
+		for(int i = 0; i < 3; i++ ) {
+			for(int j = 0; j < 3; j++ ) {
+				if( matrix[i][j] == c )
+					rowCount++;
+				if( matrix[j][i] == c )
+					columnCount++;
+				if( i == j && matrix[i][j] == c )
+					leftDiagonalCount++;
+				if( i == 3-j-1 && matrix[i][j] == c )
+					rightDiagonalCount++;
+			}
+			if( rowCount == 3 || columnCount == 3 )
+				winner = true;
 			else
-				move('X');
-		}	
+				rowCount = columnCount = 0;
+		}
+		if( rightDiagonalCount == 3 || leftDiagonalCount == 3 )
+			winner = true;
+		if( winner )
+			System.out.println( c + " Won....");
+	}
+	
+	public static void main(String args[]) {            //main
+		for(int i = 1; i <= 9; i++ ) {
+			if( winner == false ) {
+				if( i % 2 == 0 ) {
+					move('O');
+					check_Move('O');
+				}
+				else {
+					move('X');
+					check_Move('X');
+				}
+			}
+			else {
+				System.out.println("Game Over..........");
+				break;
+			}
+		}
+		if( winner == false )
+			System.out.println("Match Draw.............");
 	}
 }
